369ac0abce79eac9f160b5b62fbabb7b545f2e08
Bruno Paulino <brunojppb@gmail.com>
2014-09-03 10:56:39 -0400

    Initial Commit

---
+++ TuduList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TuduList
+//
+//  Created by Bruno Paulino on 9/3/14.
+//  Copyright (c) 2014 Bruno Paulino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
